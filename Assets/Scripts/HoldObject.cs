7b542c518ee44f2d0a388cf14657b9f844778494
Jokubas <jokubas@shape.dk>
2021-03-02 12:21:18 +0100

    started implementation for holding object

---
+++ Assets/Scripts/HoldObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldObject : MonoBehaviour
+{
+    private GameObject pickedUpObject;
+    public float interactionDistance = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("e"))
+        {
+            int layerMask = 1 << 9;
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance, layerMask))
+            {
+
+            }
+        }
+    }
+}
