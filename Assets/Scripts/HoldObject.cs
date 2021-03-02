af7d112742c368b64cd8c3dbf33fffcca3f8d44f
Jokubas <jokubas@shape.dk>
2021-03-02 12:58:22 +0100

    simple holding and releasing object mechanics made

--- Assets/Scripts/HoldObject.cs
+++ Assets/Scripts/HoldObject.cs
@@ -10,16 +10,45 @@ public class HoldObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("e"))
+        if (Input.GetKeyDown("e") && pickedUpObject == null)
         {
-            int layerMask = 1 << 9;
+            PickUp();
+        }
 
-            RaycastHit hit;
+        if (pickedUpObject != null)
+        {
+            CarryObject();
 
-            if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance, layerMask))
+            if (Input.GetKey("r"))
             {
-
+                Release();
             }
         }
+    }
+
+    void PickUp()
+    {
+        int layerMask = 1 << 8;
+
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, interactionDistance, layerMask))
+        {
+            if (hit.collider.gameObject.CompareTag("PickableObject"))
+            {
+                pickedUpObject = hit.collider.gameObject;
+                
+            }
+        }
+    }
+
+    void CarryObject()
+    {
+        pickedUpObject.transform.parent = transform;
+        pickedUpObject.transform.position = transform.position + transform.forward;
+    }
+
+    void Release()
+    {
+        pickedUpObject.transform.parent = null;
+        pickedUpObject = null;
     }
 }
