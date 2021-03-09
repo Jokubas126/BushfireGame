d6ce6ba95aee0e054d76c691087d07a687587146
Jokubas <jokubas@shape.dk>
2021-03-09 11:19:27 +0100

    retagged the prefabs of map tiles

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,11 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController controller;
     public float playerSpeed = 2.0f;
 
+    public float grassMovementCoef = 1f;
+    public float treeMovementCoef = 0.2f;
+    public float waterMovementCoef = 0.35f;
+    public float bushMovementCoef = 0.7f;
+
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
@@ -15,11 +20,33 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        controller.Move(move * Time.deltaTime * playerSpeed * GetMovementCoef());
 
         if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
         }
+    }
+
+    private float GetMovementCoef()
+    {
+        int layerMask = 1 << 9; // layer of map tile
+
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 3, layerMask))
+        {
+            Debug.Log("Raycast has hit. Tag = " + hit.collider.gameObject.tag);
+            switch (hit.collider.gameObject.tag)
+            {
+                case "Grass":
+                    return grassMovementCoef;
+                case "Bush":
+                    return bushMovementCoef;
+                case "Tree":
+                    return treeMovementCoef;
+                case "Water":
+                    return waterMovementCoef;
+            }
+        }
+        return 1f;
     }
 }
