b15e98d25d0ab40b5309cc5e23af40cd1c0432c3
Mikkel Munk <cloaked@live.dk>
2021-03-16 11:13:51 +0100

    Diagonal movement clamped to playerSpeed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = Vector3.ClampMagnitude(move, 1f);
         controller.Move(move * Time.fixedDeltaTime * playerSpeed * GetMovementCoef());
 
         if (move != Vector3.zero)
