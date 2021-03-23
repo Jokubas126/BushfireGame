6a925a0d093a4085a7dad529da1f7ed914eaceb6
latedude2 <simasc10@gmail.com>
2021-03-23 17:43:56 +0100

    Fix water collision with animals and score bugs

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,17 +7,19 @@ public class Score : MonoBehaviour
     public TMP_Text animalsDeadTextMesh;
     private int animalsDead;
     private SafeZoneLogic safeZone;
+    private int animalsAtStart;
 
     void Start()
     {
         transform.Find("Animals saved").gameObject.SetActive(true);
         transform.Find("Animals dead").gameObject.SetActive(true);
         safeZone = GameObject.Find("MapManager").transform.Find("SafetyZone(Clone)").GetComponent<SafeZoneLogic>();
+        animalsAtStart = safeZone.animalsAlive.Count;
     }
 
     void Update()
     {
-        animalsSavedTextMesh.SetText("Animals saved: {0}/{1} ", safeZone.animalsSaved, safeZone.animalsAlive.Count);
+        animalsSavedTextMesh.SetText("Animals saved: {0}/{1} ", safeZone.animalsSaved, animalsAtStart);
     }
 
     public void AnimalDied()
