7e00cf048b6d60d17a67f7aa83866ff560a235a5
Jokubas <jokubas@shape.dk>
2021-03-23 11:24:33 +0100

    created AnimalHealth script

---
+++ Assets/Scripts/Health/AnimalHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalHealth : MonoBehaviour
+{
+    [SerializeField]
+    private float health = 100f;
+    [SerializeField]
+    private float fireDamage = 10f;
+    [SerializeField]
+    private float burnReactivationTime = 0.5f;
+
+    private HealthManager healthManager;
+
+    void Start()
+    {
+        healthManager = new HealthManager(health, fireDamage, burnReactivationTime);
+    }
+
+    void Update()
+    {
+        if (healthManager.IsAllowedToBurn(transform))
+        {
+            StartCoroutine(healthManager.Burn());
+        }
+        if (healthManager.IsDead)
+        {
+            
+        }
+    }
+}
