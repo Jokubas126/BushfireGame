9aa085b4fae574e48e46184079ad8d532848b09d
Mikkel Munk <cloaked@live.dk>
2021-03-23 16:03:40 +0100

    Added script for model switch

---
+++ Assets/ModelChangeOnBurnOut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelChangeOnBurnOut : MonoBehaviour
+{
+    public TileFire tileFire;
+    public GameObject fromModel;
+    public GameObject toModel;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (tileFire.fireResistanceCurrent > 0 && tileFire.fireDuration <= 0)
+        {
+            fromModel.SetActive(false);
+            toModel.SetActive(true);
+        }
+    }
+}
