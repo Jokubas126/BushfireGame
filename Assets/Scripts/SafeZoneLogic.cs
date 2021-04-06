b70cae860abacae8216ac3e3628901772e553f91
Jokubas <jokubas@shape.dk>
2021-04-06 10:14:19 +0200

    adjusted code

--- Assets/Scripts/SafeZoneLogic.cs
+++ Assets/Scripts/SafeZoneLogic.cs
@@ -7,13 +7,15 @@ public class SafeZoneLogic : MonoBehaviour
 {
     private GameObject player;
     private GameObject waterHose;
-    private GameObject canvas;
+    private GameObject winText;
     [SerializeField]
     private int radius;
     public List<GameObject> animalsAlive;
     public int animalsSaved;
 
     private bool objectsFound = false;
+
+    private bool hasPlayerWon;
 
     void Start()
     {
@@ -22,7 +24,7 @@ public class SafeZoneLogic : MonoBehaviour
 
     void Update()
     {
-        if (objectsFound == true)
+        if (objectsFound && !hasPlayerWon)
         {
             animalsAlive = GameObject.FindGameObjectsWithTag("PickableObject").ToList();
 
@@ -38,7 +40,11 @@ public class SafeZoneLogic : MonoBehaviour
                     }
                 ).Count;
 
-            canvas.transform.Find("WinText").gameObject.SetActive(animalsSaved == animalsAlive.Count && IsTargetInRange(player, radius));
+            if (animalsSaved == animalsAlive.Count && IsTargetInRange(player, radius))
+            {
+                winText.SetActive(true);
+                hasPlayerWon = true;
+            }
         }
     }
 
@@ -52,16 +58,18 @@ public class SafeZoneLogic : MonoBehaviour
         return false;
     }
 
-    private IEnumerator LocateObjects() //All objects will have to have spawned before the safetyzone can locate them. Better solution will be to run a function from mapmanager when it's done loading all tiles.
+    //All objects will have to have spawned before the safetyzone can locate them. 
+    //Better solution will be to run a function from mapmanager when it's done loading all tiles.
+    private IEnumerator LocateObjects()
     {
         yield return new WaitForSeconds(1);
-        objectsFound = true;
         player = GameObject.FindGameObjectWithTag("Player");
         waterHose = player.transform.Find("WaterHose").gameObject;
         animalsAlive = GameObject.FindGameObjectsWithTag("PickableObject").ToList();
         Score scoreScript = GameObject.Find("Canvas").GetComponent<Score>();
         scoreScript.enabled = true;
         scoreScript.animalsAtStart = animalsAlive.Count;
-        canvas = GameObject.Find("Canvas");
+        winText = GameObject.Find("Canvas").transform.Find("WinText").gameObject;
+        objectsFound = true;
     }
 }
