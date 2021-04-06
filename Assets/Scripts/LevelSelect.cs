9a9db2dd48dc52aab038d19b72f49dab659008e5
latedude2 <simasc10@gmail.com>
2021-04-06 11:19:43 +0200

    Add level selection screen

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void LoadTutorial()
+    {
+        Debug.Log("Loading tutorial");
+        SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
+    }
+
+    public void LoadLevel1()
+    {
+        Debug.Log("Loading level 1");
+        SceneManager.LoadScene("Master", LoadSceneMode.Single);
+    }
+}
